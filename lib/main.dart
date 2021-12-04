05dab9a6c69a9c2d9ef91a30122ac3254a4d5e4e
JuanFerr98 <36459186+1214741@users.noreply.github.com>
2021-12-04 13:52:01 -0600

    casi
    tratando de llamar a la api, pero dice que no lo esta trayendo

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:mercadolibre/services/fetch.dart';
 
-Future<Album> fetchAlbum() async {
-  final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+import 'package:mercadolibre/services/product.dart';
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
-  }
-}
 
 void main() => runApp(const MyApp());
 
@@ -50,41 +16,55 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
+  late Future<Product> futureProduct;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureProduct = fetchProduct();
   }
 
   @override
   Widget build(BuildContext context) {
+    //final _controller = TextEditingController();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Fetch Data Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Fetch Data Example'),
+          title: const Text('Api Mercado Libre'),
         ),
         body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.title);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
+          /*child: ListView(children: [
+            TextField(
+              controller: _controller,
+            ),
+            const MaterialButton(
+              onPressed: null,
+              child: Text('Get Product', style: TextStyle(fontSize: 20)),
+            ),
+            
+          ]),*/
+          child: FutureBuilder<Product>(
+              future: futureProduct,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data!.title);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
 
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ),
         ),
       ),
     );
   }
 }
+
+
